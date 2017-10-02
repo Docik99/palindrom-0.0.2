3afe175354b9d4b123d4cb5e4af454b0ff3f12ea
Docik99 <32065929+Docik99@users.noreply.github.com>
2017-10-02 16:16:04 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int i, p;
+	string a , k , m;
+
+	getline(cin , a);
+	p = a.length();
+
+    for (i = p - 1; i >= 0; --i)
+    {
+		if ((a[i] > 64) && (a[i] < 91))
+        {
+            k += a[i];
+        }
+		if ((a[i] > 96) && (a[i] < 123))
+        {
+                k += a[i] - 32;
+        }
+		if ((a[i] > 47 ) && (a[i] < 58 ))
+        {
+            k += a[i];
+		}
+	}
+
+    p = k.length();
+
+    for (i = p - 1; i >= 0; i--)
+    {
+		m += k[i];
+	}
+    if (m == k)
+    {
+		cout << '1';
+	}
+	else
+    {
+		cout << '0';
+	}
+	cin.get();
+
+	return 0;
+}
